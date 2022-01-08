9558363a6b83fbcc282145239ab14258b4c178d8
zhangeric1 <86126065+zhangeric1@users.noreply.github.com>
2022-01-07 19:03:15 -0500

    Update ScreenFrame.java

--- Whack_A_Mole_Project/src/ScreenFrame.java
+++ Whack_A_Mole_Project/src/ScreenFrame.java
@@ -1,7 +1,9 @@
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
-public class ScreenFrame extends JFrame{
-	
+@SuppressWarnings("serial")
+public abstract class ScreenFrame extends JFrame{
+
 	//Constructor: Defines state/behavior that all child frames will inherit
 	public ScreenFrame(String title) {
 		super(title);
@@ -10,4 +12,61 @@ public class ScreenFrame extends JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
+	
+	/**
+	 * This method creates a custom JButton
+	 * @param text Text to be put on button
+	 * @param x X coordinate of button
+	 * @param y Y coordinate of button
+	 * @param width Width of button
+	 * @param height Height of button
+	 * @param fg Foreground color
+	 * @param bg Background color
+	 * @param font Font of button
+	 * @return Customized JButton
+	 */
+	protected JButton addCustomButton(String text, int x, int y, int width, int height, Color fg, Color bg, Font font) {
+		JButton button = new JButton(text);
+		button.setBounds(x, y, width, height);
+		button.setForeground(fg);
+		button.setBackground(bg);
+		button.setFont(font);
+		button.setFocusable(false);
+		return button;
+	}//end of addCustomButton method
+	
+	/**
+	 * This method creates a custom JButton without x and y coordinates in case the JButton is being 
+	 * placed with a layout manager
+	 * @param text Text to be put on button
+	 * @param width Width of button
+	 * @param height Height of button
+	 * @param fg Foreground color
+	 * @param bg Background color
+	 * @param font Font of button
+	 * @return Customized JButton
+	 */
+	protected JButton addCustomButton(String text, int width, int height, Color fg, Color bg, Font font) {
+		JButton button = new JButton(text);
+		button.setPreferredSize(new Dimension(width, height));
+		button.setForeground(fg);
+		button.setBackground(bg);
+		button.setFont(font);
+		button.setFocusable(false);
+		return button;
+	}//end of addCustomButton method
+	
+	/**
+	 * This method creates a custom JLabel
+	 * @param text Text to be put on label
+	 * @param fg Foreground color
+	 * @param font Font of label
+	 * @return Customized JLabel
+	 */
+	protected JLabel addCustomLabel(String text, Color fg, Font font) {
+		JLabel label = new JLabel(text);
+		label.setForeground(fg);
+		label.setFont(font);
+		return label;
+	}//end of addCustomLabel method
 }
