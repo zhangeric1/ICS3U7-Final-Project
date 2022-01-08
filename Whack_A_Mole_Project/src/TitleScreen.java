012b1d815399010842eb671a22699de4fd51ddac
zhangeric1 <86126065+zhangeric1@users.noreply.github.com>
2022-01-07 19:04:19 -0500

    Update TitleScreen.java

--- Whack_A_Mole_Project/src/TitleScreen.java
+++ Whack_A_Mole_Project/src/TitleScreen.java
@@ -5,64 +5,53 @@ import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class TitleScreen extends ScreenFrame implements ActionListener{
-	JPanel txtPanel;
-	JLabel label;
-	JButton jbtNxt;
+	private JLabel label;
+	private JButton jbtNxt;
+	private Font titleFont;
+	private Color titleColor;
 	
 	public TitleScreen(String title) {
 		super(title);
 		this.setLayout(new FlowLayout(FlowLayout.CENTER, 400, 30));
-		this.getContentPane().setBackground(Color.blue);
+		this.getContentPane().setBackground(Color.lightGray);
 		
-		txtPanel = new JPanel();
-		this.add(label = new JLabel("Final Project: Whack-a-Mole!"));
-		label.setFont(new Font("Comic Sans", Font.BOLD, 40));
-		label.setForeground(Color.green);
+		titleColor = Color.black;
+		titleFont = new Font("Comic Sans", Font.BOLD, 40);
+		this.add(label = addCustomLabel("Final Project: Whack-a-Mole!", titleColor, titleFont));
 		label.setVerticalAlignment(JLabel.CENTER);
 		label.setHorizontalAlignment(JLabel.CENTER);
 		
-		this.add(label = new JLabel("Students: Eric Zhang and Victor Zhang"));
-		label.setFont(new Font("Comic Sans", Font.BOLD, 40));
-		label.setForeground(Color.green);
+		this.add(label = addCustomLabel("Students: Eric Zhang and Victor Zhang", titleColor, titleFont));
 		label.setVerticalAlignment(JLabel.CENTER);
 		label.setHorizontalAlignment(JLabel.CENTER);
 		
-		this.add(label = new JLabel("Teacher: Ms. Xie"));
-		label.setFont(new Font("Comic Sans", Font.BOLD, 40));
-		label.setForeground(Color.green);
+		this.add(label = addCustomLabel("Teacher: Ms. Xie", titleColor, titleFont));
 		label.setVerticalAlignment(JLabel.CENTER);
 		label.setHorizontalAlignment(JLabel.CENTER);
 		
-		this.add(label = new JLabel("Jan 28, 2022"));
-		label.setFont(new Font("Comic Sans", Font.BOLD, 40));
-		label.setForeground(Color.green);
+		this.add(label = addCustomLabel("Jan 28, 2022", titleColor, titleFont));
 		label.setVerticalAlignment(JLabel.CENTER);
 		label.setHorizontalAlignment(JLabel.CENTER);
 		
-		this.add(label = new JLabel("ICS3U7-01"));
-		label.setFont(new Font("Comic Sans", Font.BOLD, 40));
-		label.setForeground(Color.green);
+		this.add(label = addCustomLabel("ICS3U7-01", titleColor, titleFont));
 		label.setVerticalAlignment(JLabel.CENTER);
 		label.setHorizontalAlignment(JLabel.CENTER);
 		
-		jbtNxt = new JButton("Next");
-		jbtNxt.setPreferredSize(new Dimension(200, 75));
-		jbtNxt.setForeground(Color.red);
-		jbtNxt.setBackground(Color.cyan);
-		jbtNxt.setFocusable(false);
-		jbtNxt.setFont(new Font("Comic Sans", Font.BOLD, 20));
+		jbtNxt = addCustomButton("Next", 200, 75, Color.black, Color.cyan, new Font("Comic Sans", Font.BOLD, 20));
 		jbtNxt.addActionListener(this);
 		this.add(jbtNxt);
 		
 		//Set Visible again because FlowLayout needs it to be set last
 		this.setVisible(true);
-		//this.add(txtPanel);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == jbtNxt) {
-			MenuScreen menu = new MenuScreen("Menu");
+			this.dispose();
+			new MenuScreen("Menu");
 		}
 	}
+	
 }
+
