e933eb67e0dc03f0155681ce34e7e845b0c67f8d
zhangeric1 <86126065+zhangeric1@users.noreply.github.com>
2022-01-07 19:03:50 -0500

    Update MenuScreen.java

--- Whack_A_Mole_Project/src/MenuScreen.java
+++ Whack_A_Mole_Project/src/MenuScreen.java
@@ -1,50 +1,87 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class MenuScreen extends ScreenFrame{
-	ImageIcon bg;
-	JButton btn1 ,btn2, btn3, btn4;
-	//JPanel canvas = new paintPanel(); //all components are put on a paintPanel which holds the background image
+public class MenuScreen extends ScreenFrame implements ActionListener{
+	private ImageIcon bg;
+	private JButton btn1 ,btn2, btn3, btn4;
+	//PaintPanel canvas = new PaintPanel(); //all components are put on a PaintPanel which holds the background image
+	
+	private JLabel bgLabel;
 	
 	/**  constructor */
 	MenuScreen(String title){
 		super(title);
-		bg = new ImageIcon("images/download.jpg");
-		//canvas = new paintPanel();
+		
+		bg = new ImageIcon("images/mole1.jpg");
+		bgLabel = addCustomLabel("Welcome to Whack-A-Mole!", Color.black, new Font("MV Boli", Font.BOLD, 50));
+		bgLabel.setIcon(bg);
+		bgLabel.setHorizontalTextPosition(JLabel.CENTER);
+		bgLabel.setVerticalTextPosition(JLabel.TOP);
+		bgLabel.setIconTextGap(150);
+		this.add(bgLabel);
+		bgLabel.setBounds(50, 0, this.getWidth() - 1, this.getHeight() - 1 );
+		this.getContentPane().setBackground(Color.lightGray);
+		
+		//canvas = new PaintPanel();
 		//this.add(canvas, BorderLayout.CENTER);
 		//canvas.setLayout(null);
-		this.setLayout(null);
-		btn1 = new JButton("Play");
-		btn1.setLocation (750, 180);
-		btn1.setSize (200, 80) ;
+		this.setLayout(null);		
+		btn1 = addCustomButton("Play", 750, 180, 200, 80, Color.blue, Color.green, new Font("Comic Sans", Font.PLAIN, 25));
+		btn1.addActionListener(this);
 		add(btn1);
-
-		btn2 = new JButton("Instructions");
-		btn2.setLocation (750, 300);
-		btn2.setSize (200, 80) ;
+		
+		btn2 = addCustomButton("Instructions", 750, 300, 200, 80, Color.blue, Color.green, new Font("Comic Sans", Font.PLAIN, 25));
+		btn2.addActionListener(this);
 		add(btn2);
 
-		btn3 = new JButton("Highscores");
-		btn3.setLocation (750, 420);
-		btn3.setSize (200, 80) ;
+		btn3 = addCustomButton("Highscores", 750, 420, 200, 80, Color.blue, Color.green, new Font("Comic Sans", Font.PLAIN, 25));
+		btn3.addActionListener(this);
 		add(btn3);
 
-		btn4 = new JButton("Exit");
-		btn4.setLocation (750, 540);
-		btn4.setSize (200, 80) ;
+		btn4 = addCustomButton("Exit", 750, 540, 200, 80, Color.blue, Color.green, new Font("Comic Sans", Font.PLAIN, 25));
+		btn4.addActionListener(this);
 		this.add(btn4);
 		
-		// Problem: without this line, the program gets weird. Instead of showing all the 4 buttons, they only appear when you hover over them, so I added this line and
-		it works okay but I don't understand the problem because this property should have been inherited from the ScreenFrame superclass.
+		
 		this.setResizable(false);
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == btn1) {
+			
+		}
+		else if(e.getSource() == btn2) {
+			
+		}
+		else if(e.getSource() == btn3) {
+			new ScoreScreen("High Scores");
+			this.dispose();
+		}
+		//exit button exits the program
+		else if(e.getSource() == btn4) {
+			this.dispose();
+		}
+	}
 	
-	//This is supposed to be an inner class to paint the background image on top of but it doesn't work for some reason
-	/*class paintPanel extends JPanel{
+	/*class PaintPanel extends JPanel{
 		protected void paintComponent(Graphics g){
 			super.paintComponent(g);
 			g.drawImage(bg.getImage(),0,0,this.getWidth(),this.getHeight(),null);
 		}
 	}*/
+	class PaintPanel extends JPanel{
+		public void paint(Graphics g){
+			Graphics2D g2D = (Graphics2D) g;
+			g2D.drawImage(bg.getImage(),0,0,this.getWidth(),this.getHeight(),null);
+		}
+	}
 
 }
