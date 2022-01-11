ede606682f049e919d72e9b38a1b69db05089f88
zhangeric1 <86126065+zhangeric1@users.noreply.github.com>
2022-01-11 11:50:14 -0500

    Update TitleScreen.java

--- Whack_A_Mole_Project/src/TitleScreen.java
+++ Whack_A_Mole_Project/src/TitleScreen.java
@@ -1,4 +1,5 @@
-* This class is the Title Screen of our game. It has information on our project name, team members, teacher,
+/**
+ * This class is the Title Screen of our game. It has information on our project name, team members, teacher,
  * date, and course code. It also has a button to transport the user to the menu screen. This screen cannot be
  * returned to and only displays once every game.
  */
@@ -12,7 +13,7 @@ public class TitleScreen extends ScreenFrame implements ActionListener{
 	//instance variables
 	private JButton jbtNxt; //Next button
 	//Constants for the title font and color so we can change them easily if needed
-	private final Font TITLE_FONT = new Font("Comic Sans", Font.BOLD, 40);
+	private final Font TITLE_FONT = new Font("Comic Sans MS", Font.BOLD, 40);
 	private final Color TITLE_COLOR = Color.black;
 	
 	//Constructor for TitleScreen
@@ -22,6 +23,9 @@ public class TitleScreen extends ScreenFrame implements ActionListener{
 		//set layout manager and background color of TitleScreen
 		this.setLayout(new FlowLayout(FlowLayout.CENTER, 400, 30));
 		this.getContentPane().setBackground(Color.lightGray);
+		
+		//this.add(new JLabel("                                                         "));
+		this.add(addCustomLabel("___________________________________", Color.blue, TITLE_FONT));
 		
 		//Add Project name as a custom label (addCustomLabel was declared in ScreenFrame class so we inherit it)
 		this.add(addCustomLabel("Final Project: Whack-a-Mole!", TITLE_COLOR, TITLE_FONT));
@@ -38,7 +42,7 @@ public class TitleScreen extends ScreenFrame implements ActionListener{
 		jbtNxt = addCustomButton("Next", 200, 75, Color.black, Color.cyan, new Font("Comic Sans", Font.BOLD, 20));
 		jbtNxt.addActionListener(this); //register action listener so actions can be performed when pressed
 		this.add(jbtNxt); //add Next button to the frame
-		
+		this.add(addCustomLabel("___________________________________", Color.blue, TITLE_FONT));		
 		//Set Visible again because FlowLayout needs it to be set last
 		this.setVisible(true);
 	}
