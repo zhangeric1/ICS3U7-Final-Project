c415478df48c8663a69d89d53fb6830d99c701b0
zhangeric1 <86126065+zhangeric1@users.noreply.github.com>
2022-01-11 14:04:25 -0500

    Update DifficultyScreen.java

--- Whack_A_Mole_Project/src/DifficultyScreen.java
+++ Whack_A_Mole_Project/src/DifficultyScreen.java
@@ -5,46 +5,41 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class DifficultyScreen extends ScreenFrame implements ActionListener{
 	private ImageIcon bg;
-	private JButton btn1 ,btn2, btn3, btn4;
-	//PaintPanel canvas = new PaintPanel(); //all components are put on a PaintPanel which holds the background image
+	private JButton btn1, btn2, btn3, btn4;
+	PaintPanel canvas = new PaintPanel(); //all components are put on a PaintPanel which holds the background image
 	
 	private JLabel bgLabel;
 	
 	/**  constructor */
-	DifficultyScreen(String title){
-		super(title);
+	DifficultyScreen(){
+		super();
 		
-		//bg = new ImageIcon("images/MBackground.jpg");
+		bg = new ImageIcon("images/MBackground.jpg");
+		canvas = new PaintPanel();
+		canvas.setLayout(null);
+		this.add(canvas);
+		
 		bgLabel = addCustomLabel("Select The Difficulty", Color.black, new Font("MV Boli", Font.BOLD, 50));
-		bgLabel.setIcon(bg);
-		bgLabel.setHorizontalTextPosition(JLabel.CENTER);
-		bgLabel.setVerticalTextPosition(JLabel.TOP);
-		bgLabel.setIconTextGap(190);
-		this.add(bgLabel);
 		bgLabel.setBounds(100, -200, this.getWidth() - 1, this.getHeight() - 1 );
-		this.getContentPane().setBackground(Color.lightGray);
+		canvas.add(bgLabel);
 		
-		//canvas = new PaintPanel();
-		//this.add(canvas, BorderLayout.CENTER);
-		//canvas.setLayout(null);
-		this.setLayout(null);		
 		btn1 = addCustomButton("Easy", 25, 300, 300, 200, Color.blue, Color.green, new Font("Comic Sans", Font.PLAIN, 25));
 		btn1.addActionListener(this);
-		add(btn1);
+		canvas.add(btn1);
 		
 		btn2 = addCustomButton("Medium", 350, 300, 300, 200, Color.blue, Color.green, new Font("Comic Sans", Font.PLAIN, 25));
 		btn2.addActionListener(this);
-		add(btn2);
+		canvas.add(btn2);
 		
 		btn3 = addCustomButton("Hard", 675, 300, 300, 200, Color.blue, Color.green, new Font("Comic Sans", Font.PLAIN, 25));
 		btn3.addActionListener(this);
-		add(btn3);
+		canvas.add(btn3);
 		
 		btn4 = addCustomButton("Back", 750, 650, 200, 80, Color.blue, Color.green, new Font("Comic Sans", Font.PLAIN, 25));
 		btn4.addActionListener(this);
-		add(btn4);
+		canvas.add(btn4);
 		
-		this.setResizable(false);
+		//this.setResizable(false);
 	}
 
 	@Override
@@ -59,22 +54,16 @@ public class DifficultyScreen extends ScreenFrame implements ActionListener{
 			
 		}
 		else if(e.getSource() == btn4) {
-			new MenuScreen("Main Menu");
+			new MenuScreen();
 			this.dispose();
 		}
 
 	}
 	
-	/*class PaintPanel extends JPanel{
+	class PaintPanel extends JPanel{
 		protected void paintComponent(Graphics g){
 			super.paintComponent(g);
 			g.drawImage(bg.getImage(),0,0,this.getWidth(),this.getHeight(),null);
-		}
-	}*/
-	class PaintPanel extends JPanel{
-		public void paint(Graphics g){
-			Graphics2D g2D = (Graphics2D) g;
-			g2D.drawImage(bg.getImage(),0,0,this.getWidth(),this.getHeight(),null);
 		}
 	}
 
