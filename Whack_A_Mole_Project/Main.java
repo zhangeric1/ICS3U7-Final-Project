ae1ac290a97e0b9cc60ef7c9191aaadc3d9fcdd8
zhangeric1 <86126065+zhangeric1@users.noreply.github.com>
2022-01-27 14:39:17 -0500

    Delete Main.java

--- Whack_A_Mole_Project/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * This is our Main class where everything begins. It calls an instance of TitleScreen and the game starts 
- */
-public class Main {
-	public static void main(String[] args) {
-		//create new instance of TitleScreen
-		new TitleScreen();
-	}//end of main method
-}
